24d5c270c1502f4dbf0f19e3737fea8cf36e75c8
Eva Barrett <Evabarrett23@Gmail.com>
2018-06-26 00:51:15 -0700

    finished GET /search

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ var session = require('express-session');
 var passport = require('./config/passportConfig');
 var isLoggedIn = require('./middleware/isLoggedIn');
 var flash = require('connect-flash');
+var unirest = require('unirest');
 
 var app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 app.use(express.static(__dirname + '/public'));
@@ -45,6 +47,21 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.get('/search', function(req, res) {
+  var results = null;
+  unirest.get("https://trailapi-trailapi.p.mashape.com/?limit=10&q[city_cont]=" + req.query.city + "&q[state_cont]=" +
+  req.query.state)
+    .header("X-Mashape-Key", process.env.API_KEY)
+    .header("Accept", "text/plain")
+    .end(function (result) {
+      results = result.body.places
+      console.log('results------------', results);
+      res.render('searchResults', {searchResults: results});
+    });
+    // pass data to the front
+    // console.log('results-----', results);
+  // res.render('searchResults', {searchResults: results});
+});
 
 // Routes
 app.use('/auth', require('./controllers/auth'));
