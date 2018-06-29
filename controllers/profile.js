c0aeb8a3eb76535dd6e1560dacbae853b8e6e914
Eva Barrett <Evabarrett23@Gmail.com>
2018-06-29 16:22:31 -0700

    saved all fav on profile page

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var db = require('../models');
-var passport = require('../config/passportConfig');
 var router = express.Router();
+var request = require('request');
+var passport = require('../config/passportConfig');
 var isLoggedIn = require('../middleware/isLoggedIn');
 
 
@@ -14,11 +15,22 @@ router.get('/', isLoggedIn, function(req, res) {
     // use user b/c we found a user
   }).then(function(user) {
     user.getTrails().then(function(trails) {
-// %%%%%%%%%%requery the api for each id!!%%%%%%next step
-
-      res.render('profile/profile', {
-        user: user,
-        trails: trails
+      // Requery the API with selected id's
+      var trailsIdArray = [];
+      trails.forEach(function(trail) {
+        trailsIdArray.push(trail.uniqueTrailId);
+      });
+      var idString = trailsIdArray.join(',');
+      console.log(idString);
+      request("https://www.hikingproject.com/data/get-trails-by-id?ids=" + idString + "&key=" + process.env.HIKING_PROJECT_KEY, function(error, response, body) {
+        // parse body, data --> something we can work with, data we got back from API, in an OBJECT form b/c of JSON.parse (was in JSON see w3schools)
+        var data = JSON.parse(body);
+        console.log('##############################################################');
+        console.log('data', data);
+        res.render('profile/profile', {
+          user: user,
+          trails: data.trails
+        });
       });
     });
   });
